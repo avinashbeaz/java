9ca8af51511630353ff8570e384fd24e760a0851
avinashbeaz <avinashbeaz@gmail.com>
2023-08-07 15:23:25 +0530

    scanner class

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello World");
-        System.out.print("Avinash");
-        System.out.print(" Kumar");
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextInt());
 
     }
 }
