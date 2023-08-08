e1c1ff9b27afbf6d91dd649f83d098dcd34b7d80
avinashbeaz <avinashbeaz@gmail.com>
2023-08-08 07:53:22 +0530

    while loop

--- Main.java
+++ Main.java
@@ -3,10 +3,36 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+         int n = input.nextInt();
+         /*
+           Syntax:
+          for(initial value;condition;increment)
+          {
+             body or process
+          }
 
-       for(int num = 1; num < 9; num += 2) {
+          */
+
+
+       for(int num = 1; num < n; num += 2) {
            System.out.println(num);
        }
+
+       /*
+           Syntax:
+           initial value;
+          while(condition)
+          {
+             body or process;
+             increment;
+          }
+
+           */
+        int num2 =0;
+        while( num2 < 20) {
+            num2 = num2 +2;
+            System.out.println(num2);
+        }
 
     }
 
