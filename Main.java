36720d8a36eec0493aa07c35c5f370eb640826c0
avinashbeaz <avinashbeaz@gmail.com>
2023-08-08 07:36:41 +0530

    nested if else

--- Main.java
+++ Main.java
@@ -4,11 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int employeeSalary = input.nextInt();
-       if (employeeSalary > 20000) {
-           employeeSalary = employeeSalary + 1000;
-       } else employeeSalary = employeeSalary + 5000;
-       System.out.println(employeeSalary);
+        int employeeAge = input.nextInt();
+       if (employeeAge > 60) {
+           System.out.println("you are retired");
+       } else if (employeeAge >20) {
+           System.out.println("good luck for your job , you have " + (60 -employeeAge) + "years left  to retire");
+       } else System.out.println("you are too young to do this job");
+
     }
 
 }
