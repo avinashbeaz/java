ba7bfd67c825d151848f5bdc7fbd19925cd88393
avinashbeaz <avinashbeaz@gmail.com>
2023-08-08 07:42:41 +0530

    for loop

--- Main.java
+++ Main.java
@@ -4,12 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int employeeAge = input.nextInt();
-       if (employeeAge > 60) {
-           System.out.println("you are retired");
-       } else if (employeeAge >20) {
-           System.out.println("good luck for your job , you have " + (60 -employeeAge) + "years left  to retire");
-       } else System.out.println("you are too young to do this job");
+       for(int num = 1; num < 9; num += 2) {
+           System.out.println(num);
+       }
 
     }
 
