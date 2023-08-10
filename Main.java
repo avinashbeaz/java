b52bf2c76096d24cdaca06da85c85a3a9cdd2beb
avinashbeaz <avinashbeaz@gmail.com>
2023-08-10 22:08:23 +0530

    switch statements

--- Main.java
+++ Main.java
@@ -3,15 +3,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("enter random number");
-        int n = input.nextInt();
-        int m =0;
-        while (n > 0){
-            int rem = n % 10;
-            n=n/10;
-            m = m*10 + rem;
-        }
-        System.out.println(m);
+        System.out.println("enter engine cc");
+        int engine = input.nextInt();
+       /*
+       switch syntax:
+       switch (expression) {
+       case one:
+         //do something
+         break;
+          case two:
+         //do something
+         break;
+         default:
+         //do something
+         }
 
+        */
+        switch (engine) {
+            case 150 -> System.out.println("you have good mileage");
+            case 200 -> System.out.println("you have decent pickup");
+            case 390 -> System.out.println("the ktm dude");
+            case 1000 -> System.out.println("i can hear you from far away");
+            default -> System.out.println("don't try to be smart enter details correctly");
+        }
     }
 }
