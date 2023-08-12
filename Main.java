504ed644a4735a8c780e1641de10cc3bf6055b0f
avinashbeaz <avinashbeaz@gmail.com>
2023-08-12 16:07:09 +0530

    armstrong number

--- Main.java
+++ Main.java
@@ -2,27 +2,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int d;
-        do {
-            System.out.println("enter first number ");
-            int a = in.nextInt();
-            System.out.println("enter second number ");
-            int b = in.nextInt();
-            System.out.println("enter 1 to add,2 to sub,3 to mul, 4 to div ");
-            int c = in.nextInt();
-            int e = 0;
-            switch (c) {
-                case 1 -> e = Calculator.sum(a, b);
-                case 2 -> e = Calculator.sub(a, b);
-                case 3 -> e = Calculator.mul(a, b);
-                case 4 -> e = Calculator.div(a, b);
-                default -> System.out.println("enter valid operations");
-            }
-
-            System.out.println(e);
-            System.out.println("do you want to calculate press 0 ,press other key to exit");
-            d = in.nextInt();
-        } while(d==0);
+        Scanner in =new Scanner(System.in);
+        System.out.println("enter number");
+        int x = in.nextInt();
+        if (x == armStrong(x))
+        {
+            System.out.print("your number is armstrong");
+        }
+        else
+        {
+            System.out.print("your number is not  armstrong");
+        }
     }
+
+    private static int armStrong(int x) {
+     int v=0;
+        while (x> 0){
+            int rem = x% 10;
+            v = rem*rem*rem +v;
+            x=x/10;
+        }
+        return v;
 }
+}
+
