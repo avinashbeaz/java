2b40b197325bee3c9c238a89a9bd7794de275ea9
avinashbeaz <avinashbeaz@gmail.com>
2023-08-15 12:07:18 +0530

    max min of 3 number

--- Main.java
+++ Main.java
@@ -9,14 +9,30 @@ public class Main {
         int b = in.nextInt();
         System.out.println("enter third number");
         int c = in.nextInt();
+        System.out.println("max number is :" +max(a,b,c)); System.out.println("min mber is :" +min(a,b,c));
+    }
+    static int max(int a, int b , int c) {
+        int max;
         if (a > b && b > c) {
-            System.out.println(a);
+           max = a;
         } else if(b > a && b>c ){
-            System.out.println(b);
+            max = b;
 
         }else {
-            System.out.println(c);
+         max = c;
         }
+        return max;
+    }
+    static int min(int a, int b , int c) {
+        int min;
+        if (a < b && b < c) {
+            min = a;
+        } else if(b < a && b<c ){
+            min = b;
+
+        }else {
+            min = c;
+        }
+        return min;
     }
 }
-
