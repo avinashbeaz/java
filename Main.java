a39bc7991de9a93f8fd67d1b48d7e923f8e9a2fa
avinashbeaz <avinashbeaz@gmail.com>
2023-08-29 13:10:01 +0530

    same number in array

--- Main.java
+++ Main.java
@@ -1,38 +1,22 @@
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("enter first number");
-        int a = in.nextInt();
-        System.out.println("enter second number");
-        int b = in.nextInt();
-        System.out.println("enter third number");
-        int c = in.nextInt();
-        System.out.println("max number is :" +max(a,b,c)); System.out.println("min mber is :" +min(a,b,c));
-    }
-    static int max(int a, int b , int c) {
-        int max;
-        if (a > b && b > c) {
-           max = a;
-        } else if(b > a && b>c ){
-            max = b;
 
-        }else {
-         max = c;
-        }
-        return max;
-    }
-    static int min(int a, int b , int c) {
-        int min;
-        if (a < b && b < c) {
-            min = a;
-        } else if(b < a && b<c ){
-            min = b;
+        int[] arr = {23,34,56,34,65};
+        int target = 34;
+        System.out.println(same(arr,target));
 
-        }else {
-            min = c;
-        }
-        return min;
+    }
+
+ static int same(int[] arr,int target) {
+     int count = 0;
+     for (int element: arr) {
+
+         if (element == target) {
+             count++;
+         }
+     }
+     return count;
+
     }
 }
